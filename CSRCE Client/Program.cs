67498c5cde6613b8b8781059d3237011b264e97c
KadePcGames <kadepcgames@gmail.com>
2019-10-17 18:15:02 -0700

    Added code :P

---
+++ CSRCE Client/Program.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSRCE_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string ip = "127.0.0.1";
+            int port = 1337;
+
+            // While it is able to make this console invisable, for now we shouldn't.
+            Console.WriteLine("[!] This is a program that can potentially cause harm to your device, depending on the vendor.\n[!] Only install this from trusted sources!\n[!] Description: This program can execute remote code, which can be harmful.");
+            Console.WriteLine("[!] Enter 'y' to accept.");
+            bool answer = Console.ReadLine().ToLower() == "y";
+            while (answer)
+            {
+                NetworkStream stream = null;
+                try
+                {
+                    stream = Connect(ip, port);
+                }
+                catch { }
+
+                if (stream != null)
+                {
+                    Console.WriteLine("[!] Connected to master server, running script...");
+
+                    string code = Recieve(stream);
+
+                    CSharpCodeProvider provider = new CSharpCodeProvider();
+                    CompilerParameters parameters = new CompilerParameters();
+
+                    if (code.Contains("Form"))
+                    {
+                        parameters.ReferencedAssemblies.Add("System.Windows.Forms.dll");
+                        parameters.ReferencedAssemblies.Add("System.dll");
+                    }
+                    if (code.Contains("Drawing"))
+                        parameters.ReferencedAssemblies.Add("System.Drawing.dll");
+
+                    parameters.GenerateInMemory = true;
+                    parameters.GenerateExecutable = true;
+
+                    CompilerResults results = provider.CompileAssemblyFromSource(parameters, code);
+
+                    if (results.Errors.HasErrors)
+                    {
+                        StringBuilder sb = new StringBuilder();
+
+                        foreach (CompilerError error in results.Errors)
+                        {
+                            sb.AppendLine(string.Format("Error ({0}): {1} , Line: {2}", error.ErrorNumber, error.ErrorText, error.Line));
+                        }
+
+                        Console.WriteLine(sb.ToString());
+
+                        Console.Read();
+                        answer = false;
+                    }
+                    else
+                    {
+                        Assembly assembly = results.CompiledAssembly;
+                        Type program = assembly.GetType("Program.Program");
+                        MethodInfo main = program.GetMethod("Main");
+
+                        main.Invoke(null, null);
+
+                        answer = false;
+                    }
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        /// <summary>
+        /// Basic connect method
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="port"></param>
+        /// <returns>NetworkStream</returns>
+
+        public static NetworkStream Connect(string ip, int port)
+        {
+            TcpClient client = new TcpClient(ip, port);
+
+            NetworkStream stream = client.GetStream();
+
+            return stream;
+        }
+
+        /// <summary>
+        /// Basic send data method
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="data"></param>
+        /// <returns>Boolean</returns>
+
+        public static bool SendData(NetworkStream stream, string data)
+        {
+            try
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(data + "\n");
+
+                stream.Write(bytes, 0, bytes.Length);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Basic receive data :)
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns>String</returns>
+        
+        public static string Recieve(NetworkStream stream)
+        {
+            try
+            {
+                byte[] bytes = new byte[1024];
+
+                stream.Read(bytes, 0, bytes.Length);
+
+                string data = Encoding.UTF8.GetString(bytes);
+
+                stream.Flush();
+
+                return data;
+            }
+            catch (Exception)
+            {
+                return "No Code";
+            }
+
+        }
+    }
+}
---
+++ CSharpRemoteCodeExecution/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CSharpRemoteCodeExecution
+{
+    class Program
+    {
+        static string filePath = "";
+
+        static void Main(string[] args)
+        {
+            // Check for a file in args
+            if (args.Length != 1)
+                return;
+            if (!File.Exists(args[0]))
+                return;
+
+            filePath = args[0];
+
+            TcpListener listener = new TcpListener(IPAddress.Any, 1337);
+            listener.Start();
+
+            Console.Title = "CSRCE Server - - - Made for NON-Malicous use!";
+
+            Console.WriteLine("[!] Waiting for a connection...");
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                ThreadPool.QueueUserWorkItem(DispatchScript, client);
+            }
+        }
+
+        static void DispatchScript(object client)
+        {
+            // Just send the script
+            Console.WriteLine("[!] Client connected, dispatching script...");
+
+            TcpClient cl = (TcpClient)client;
+
+            SendData(cl.GetStream(), File.ReadAllText(filePath));
+
+        }
+
+        /// <summary>
+        /// Basic send data method
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="data"></param>
+        /// <returns>Boolean</returns>
+
+        public static bool SendData(NetworkStream stream, string data)
+        {
+            try
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(data);
+
+                stream.Write(bytes, 0, bytes.Length);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+    }
+}
